086f51f56414670a5ecf4cdb758b34dc72a83f27
Zehra ikizler <zehraaikizler@gmail.com>
2021-12-24 23:13:47 +0300

    like-unlike eklendi

--- script.js
+++ script.js
@@ -32,12 +32,12 @@ fetch("products.json").then((response) => {
             div.innerHTML = `
                 <div class="card shadow-lg border-0 p-3">
                     <div class="favorite-product position-absolute end-0 me-4 mt-2 bg-white rounded-circle py-1 px-2">
-                        <span class="d-block">
+                        <span class="unlike-heart" style="display: block;">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
                                 <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
                             </svg>
                         </span>
-                        <span class="d-none">
+                        <span class="like-heart" style="display: none;">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-heart-fill" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                             </svg>
@@ -60,5 +60,21 @@ fetch("products.json").then((response) => {
 
             productRow.appendChild(div);
         };
+
+        // like - unlike heart
+        let likeHeart = document.querySelectorAll('.like-heart');
+        let unlikeHeart = document.querySelectorAll('.unlike-heart');
+        unlikeHeart.forEach(function(el, index) {
+            el.addEventListener("click", () => {
+                unlikeHeart[index].style.display = "none";
+                likeHeart[index].style.display = "block";
+            });
+        });
+        likeHeart.forEach(function(el, index){
+            el.addEventListener("click", () => {
+                likeHeart[index].style.display = "none";
+                unlikeHeart[index].style.display = "block";
+            });
+        });
     });
 });
